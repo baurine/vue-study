232a0dceae2efd27b6eb85f6f42ac1e409ba1099
baurine <baurine@ekohe.com>
2016-07-22 13:41:45 +0800

    feat: vue wit api, part 2, client side

--- step_by_step/10_vue_api_fetch_data/js/main.js
+++ step_by_step/10_vue_api_fetch_data/js/main.js
@@ -1,45 +1,40 @@
 Vue.component('tasks', {
   template: "#tasks-template",
 
-  props: ['list'],
-
-  computed: {
-    remaining: function() {
-      return this.list.filter(this.isProgress).length
+  data: function() {
+    return {
+      list: []
     }
   },
 
+  created: function() {
+    // var vm = this;
+    // $.getJSON('api/todos', function(todos) {
+    //   console.log(todos);
+    //   vm.list = todos;
+    // });
+
+    // or
+    // $.getJSON('api/todos', function(todos) {
+    //   this.list = todos;
+    // }.bind(this));
+
+    this.fetchTasks();
+  },
+
   methods: {
-    toggleTask: function(task) {
-      task.completed = !task.completed
+    fetchTasks: function() {
+      $.getJSON('api/todos', function(todos) {
+        this.list = todos;
+      }.bind(this));
     },
 
-    isCompleted: function(task) {
-      return task.completed
-    },
-
-    isProgress: function(task) {
-      return !this.isCompleted(task)
-    },
-
-    deleteTask: function(task) {
-      this.list.$remove(task)
-    },
-
-    clearCompleted: function() {
-      this.list = this.list.filter(this.isProgress)
+    delete: function(task) {
+      this.list.$remove(task);
     }
   }
-})
+});
 
 new Vue({
   el: "#app",
-
-  data: {
-    tasks: [
-      { body: "Go to work", completed: false },
-      { body: "Go to bank", completed: false },
-      { body: "Go to mall", completed: true },
-    ]
-  }
-})
+});
