d3ca8caabf0c37e1791e74fa89bb9d1d46a3ec93
baurine <2008.hbl@gmail.com>
2019-05-13 09:55:55 +0800

    design+code vue chapter 9 - charts 1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>&nbsp;|
-    <router-link to="/about">About</router-link>&nbsp;|
-    <a @click="logOut">Logout</a>
+  <div id="nav" :class="{'nav-light': !isDarkMode, 'nav-dark': isDarkMode}">
+    <div class="nav-1">
+      <img src="@/assets/DCHQ-small.svg">
+      <router-link to="/" :class="{'light-nav': !isDarkMode, 'dark-nav': isDarkMode}">Home</router-link>
+      <router-link to="/team" :class="{'light-nav': !isDarkMode, 'dark-nav': isDarkMode}">Team</router-link>
+    </div>
+    <a @click="logOut">
+      Logout
+      <img src="@/assets/logout.svg">
+    </a>
   </div>
 </template>
 
 <script>
 export default {
   name: "Header",
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  },
   methods: {
     logOut() {
       this.$router.push({
@@ -22,4 +33,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  padding: 15px 15%;
+  width: 100%;
+
+  a {
+    font-weight: bold;
+    color: $dark-gray;
+    text-decoration: none;
+    cursor: pointer;
+
+    &.router-link-exact-active.dark-nav {
+      color: $white;
+    }
+    &.router-link-exact-active.light-nav {
+      color: $middle-blue;
+    }
+  }
+}
+.nav-light {
+  background: $white;
+}
+.nav-dark {
+  background: $super-dark-blue;
+}
+
+.nav-1 {
+  display: flex;
+  align-items: center;
+
+  a {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+  img {
+    margin-right: 20px;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="home">
     <Header/>
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="spread">
+        <h3>Traffic Overview</h3>
+        <div class="toggle">
+          <div
+            class="interval-tab"
+            :class="item === currentInterval && 'active-tab'"
+            v-for="item in trafficIntervals"
+            :key="item"
+            @click="currentInterval = item"
+          >{{item}}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Header from "@/components/Header.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      trafficIntervals: ["Days", "Weeks", "Months"],
+      currentInterval: "Days"
+    };
+  },
   components: {
-    HelloWorld,
     Header
   }
 };
@@ -23,5 +39,45 @@ export default {
 <style lang="scss" scoped>
 .home {
   text-align: center;
+}
+.container {
+  padding: 0 15%;
+}
+.spread {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 40px;
+}
+h3 {
+  @include heading-3;
+}
+.toggle {
+  @include medium-text;
+  display: flex;
+  flex-wrap: nowrap;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 6px;
+  width: 240px;
+  height: 50px;
+  color: $dark-gray;
+  padding: 5px;
+  background: rgba(255, 255, 255, 0.1);
+}
+.interval-tab {
+  width: 33.33%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+.active-tab {
+  background: $teal;
+  border-radius: 4px;
+  color: white;
 }
 </style>
